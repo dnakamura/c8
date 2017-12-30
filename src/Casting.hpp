62d36668df213c46319d1a9ca295cc48715568f8
Devin Nakamura <devinn@ca.ibm.com>
2017-12-30 10:34:29 -0500

    Clang-format cleanup

--- src/Casting.hpp
+++ src/Casting.hpp
@@ -1,46 +1,47 @@
-#include <type_traits>
 #include <assert.h>
+#include <type_traits>
 
-template<class X, class Y, typename Enabler = void>
+template <class X, class Y, typename Enabler = void>
 struct isa_impl {
-	static inline bool impl(const Y * ptr) {
-		assert(ptr && "isa<> called on null pointer");
-		return X::classof(ptr);
-	}
+  static inline bool impl(const Y* ptr) {
+    assert(ptr && "isa<> called on null pointer");
+    return X::classof(ptr);
+  }
 };
 
-template<class X, class Y>
-struct isa_impl<X, Y, typename std::enable_if<std::is_base_of<X, Y>::value>::type> {
-	static inline bool impl(const Y* ptr) { 
-		assert(ptr && "isa<> called on null pointer");
-		return true; 
-	}
-
+template <class X, class Y>
+struct isa_impl<X, Y,
+                typename std::enable_if<std::is_base_of<X, Y>::value>::type> {
+  static inline bool impl(const Y* ptr) {
+    assert(ptr && "isa<> called on null pointer");
+    return true;
+  }
 };
 /*
 template<class X, class Y, typename Enabler = void>
 struct isa_impl {
-	static inline bool impl(const Y& obj) {
-		assert(ptr && "isa<> called on null pointer");
-		return X::classof(obj);
-	}
+        static inline bool impl(const Y& obj) {
+                assert(ptr && "isa<> called on null pointer");
+                return X::classof(obj);
+        }
 };
 
 template<class X, class Y>
-struct isa_impl<X, Y, typename std::enable_if<std::is_base_of<X, Y>::value>::type> {
-	static inline bool impl(const Y& ptr) { 
-		assert(ptr && "isa<> called on null pointer");
-		return true; 
-	}
+struct isa_impl<X, Y, typename std::enable_if<std::is_base_of<X,
+Y>::value>::type> {
+        static inline bool impl(const Y& ptr) {
+                assert(ptr && "isa<> called on null pointer");
+                return true;
+        }
 
 };*/
 
-template<class X, class Y>
-inline bool isa(const Y * ptr) {
-	return isa_impl<X, Y>::impl(ptr);
+template <class X, class Y>
+inline bool isa(const Y* ptr) {
+  return isa_impl<X, Y>::impl(ptr);
 }
 /*
 template<class X, class Y>
 inline bool isa(const Y & obj) {
-	return isa_impl<X, Y>::impl(obj);
+        return isa_impl<X, Y>::impl(obj);
 }*/
--- src/main.cpp
+++ src/main.cpp
@@ -1,45 +1,45 @@
+#include "Debug.hpp"
 #include "Lexer.hpp"
 #include "Parser.hpp"
-#include "Debug.hpp"
 
+#include <lingo/character.hpp>
+#include <lingo/error.hpp>
 #include <lingo/file.hpp>
 #include <lingo/io.hpp>
-#include <lingo/error.hpp>
-#include <lingo/character.hpp>
 
-#include <iostream>
 #include <exception>
+#include <iostream>
 
 using namespace c8;
 
 int main(int argc, char **argv) {
-    std::set_terminate(__gnu_cxx::__verbose_terminate_handler);
-     std::cout << "\n\n";
-    if (argc != 2) {
-        std::cerr << "usage: c8 <input-file>\n";
-        return -1;
-    }
+  std::set_terminate(__gnu_cxx::__verbose_terminate_handler);
+  std::cout << "\n\n";
+  if (argc != 2) {
+    std::cerr << "usage: c8 <input-file>\n";
+    return -1;
+  }
 
-    lingo::File input(argv[1]);
-    lingo::Character_stream cs(input);
-    lingo::Token_stream ts;
+  lingo::File input(argv[1]);
+  lingo::Character_stream cs(input);
+  lingo::Token_stream ts;
 
-    c8::Lexer lex(cs,ts);
+  c8::Lexer lex(cs, ts);
 
-    lex();
-    Parser parser(ts);
+  lex();
+  Parser parser(ts);
 
-    std::cout << "Finished lexing\n";
+  std::cout << "Finished lexing\n";
 
-    //lingo::Token t = ts.get();
-    /*while(! ts.eof()) {
-        std::cout << ts.get() << "\n";
-    }*/
-    ast::NodePtr<ast::FunctionDeclaration> funcptr = parser.ParseFunctionDef();
-    ast::DebugPrinter dbg;
-    dbg.Visit(funcptr.get());
-    //lingo::Printer printer(std::cout);
-    //dbg::Dump(printer,*funcptr);
+  // lingo::Token t = ts.get();
+  /*while(! ts.eof()) {
+      std::cout << ts.get() << "\n";
+  }*/
+  ast::NodePtr<ast::FunctionDeclaration> funcptr = parser.ParseFunctionDef();
+  ast::DebugPrinter dbg;
+  dbg.Visit(funcptr.get());
+  // lingo::Printer printer(std::cout);
+  // dbg::Dump(printer,*funcptr);
 
-    return 0;
+  return 0;
 }
