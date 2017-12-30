9e684d3465d29c6953aa7a01274a3b6587e8391f
Devin Nakamura <devinn@ca.ibm.com>
2017-12-30 14:18:19 -0500

    Dead code cleanup

--- src/Casting.hpp
+++ src/Casting.hpp
@@ -17,31 +17,8 @@ struct isa_impl<X, Y,
     return true;
   }
 };
-/*
-template<class X, class Y, typename Enabler = void>
-struct isa_impl {
-        static inline bool impl(const Y& obj) {
-                assert(ptr && "isa<> called on null pointer");
-                return X::classof(obj);
-        }
-};
-
-template<class X, class Y>
-struct isa_impl<X, Y, typename std::enable_if<std::is_base_of<X,
-Y>::value>::type> {
-        static inline bool impl(const Y& ptr) {
-                assert(ptr && "isa<> called on null pointer");
-                return true;
-        }
-
-};*/
 
 template <class X, class Y>
 inline bool isa(const Y* ptr) {
   return isa_impl<X, Y>::impl(ptr);
 }
-/*
-template<class X, class Y>
-inline bool isa(const Y & obj) {
-        return isa_impl<X, Y>::impl(obj);
-}*/
